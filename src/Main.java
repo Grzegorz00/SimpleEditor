26e668d5ff4020f927230651c7317265b5ede461
Grzesiek <g.swiecickii@gmail.com>
2020-06-05 20:54:17 +0200

    Setting JFrame

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world test");
+        new Main();
+    }
+
+    public Main(){
+        SwingUtilities.invokeLater(() -> createGUI());
+    }
+
+    protected void createGUI()
+    {
+        JFrame jf = new JFrame();
+        jf.setTitle("Simple editor - ?????????");
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setLocationRelativeTo(null);
+        jf.setLayout(new BorderLayout());
+
+        jf.setVisible(true);
     }
 }
